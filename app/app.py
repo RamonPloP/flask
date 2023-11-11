26ae97ed2586915db083f24ddd33b448d1fc9fad
RamonPloP <aspireandlive@gmail.com>
2023-11-11 15:28:32 -0600

    Add de Tags

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ db = client.blog
 
 # Declaracion para coleccion users
 users_col = db.users
+tags_col = db.tags
 
 app = Flask(__name__)
 
@@ -27,10 +28,9 @@ def users():
     }
     return render_template('/users.html', data=data)
 
-@app.route('/users/add', methods =['GET','POST'])
-def add():
+@app.route('/users/adduser', methods =['GET','POST'])
+def addUser():
     if request.method == 'POST':
-        print('dentro de if')
         name = request.form['name']
         email = request.form['email']
         user = {
@@ -38,15 +38,14 @@ def add():
             "email": email
         }
         users_col.insert_one(user)
-        print(user)
-        return redirect(url_for('add'))
+        return redirect(url_for('addUser'))
     data = {
         "title": "Agregar usuario",
         "db": users
     }
     return render_template('/users/add.html',data=data)
 
-@app.route('/users/list')
+@app.route('/users/listusers')
 def listUsers():
     lista = list(users_col.find())
     data={
@@ -55,22 +54,22 @@ def listUsers():
     }
     return render_template('/users/list.html',data=data)
 
-@app.route('/users/edit', methods=['POST','GET'])
-def edit():
+@app.route('/users/edituser', methods=['POST','GET'])
+def editUser():
     if request.method == 'POST':
         user_id = request.form['id']
         new_name = request.form['name']
         new_email = request.form['email']
         users_col.update_one({"_id": ObjectId(user_id)}, {"$set": {"name": new_name, "email": new_email}})
-        return redirect(url_for('edit'))
+        return redirect(url_for('editUser'))
     data = {
         "title": "Editar Usuario"
     }
     return render_template('/users/edit.html', data=data)
 
 
-@app.route('/users/search', methods=['POST','GET'])
-def search():
+@app.route('/users/searchuser', methods=['POST','GET'])
+def searchUser():
     if request.method == 'POST':
         id = request.form['id']
         user = users_col.find_one({"_id": ObjectId(id)})
@@ -78,19 +77,94 @@ def search():
             return jsonify({'name': user['name'], 'email': user['email'], 'id': id})
         else:
             return jsonify({'error': 'Usuario no encontrado'})
-            
-@app.route('/users/delete', methods=['GET','DELETE'])
-def delete():
+
+@app.route('/users/deleteuser', methods=['GET','DELETE'])
+def deleteUser():
     if request.method == 'DELETE':
         id = request.form['id']
         users_col.delete_one({"_id": ObjectId(id)})
-        return redirect(url_for('delete'))
+        return redirect(url_for('deleteUser'))
     data={
         "title": "Eliminar Usuario",
         "db": users
     }
     return render_template('/users/delete.html', data=data)
 #---------------------------------------
+
+
+# Tags ---------------------------------------------
+
+@app.route('/tags')
+def tags():
+    data = {
+        "title": "Tags"
+    }
+    return render_template('/tags.html', data=data)
+
+@app.route('/tags/addtag', methods =['GET','POST'])
+def addTag():
+    if request.method == 'POST':
+        name = request.form['name']
+        url = request.form['url']
+        tag = {
+            "name": name,
+            "url": url
+        }
+        tags_col.insert_one(tag)
+        return redirect(url_for('addTag'))
+    data = {
+        "title": "Agregar Tag",
+        "db": users
+    }
+    return render_template('/tags/add.html',data=data)
+
+@app.route('/tags/listtags')
+def listTags():
+    lista = list(tags_col.find())
+    data={
+        "title": "Listar Tag",
+        "db": lista
+    }
+    return render_template('/tags/list.html',data=data)
+
+@app.route('/tags/edittag', methods=['POST','GET'])
+def editTag():
+    if request.method == 'POST':
+        tag_id = request.form['id']
+        new_name = request.form['name']
+        new_url = request.form['url']
+        tags_col.update_one({"_id": ObjectId(tag_id)}, {"$set": {"name": new_name, "tag": new_url}})
+        return redirect(url_for('editTag'))
+    data = {
+        "title": "Editar Tag"
+    }
+    return render_template('/tags/edit.html', data=data)
+
+
+@app.route('/tags/searchtag', methods=['POST','GET'])
+def searchTag():
+    if request.method == 'POST':
+        id = request.form['id']
+        tag = tags_col.find_one({"_id": ObjectId(id)})
+        if tag:
+            return jsonify({'name': tag['name'], 'url': tag['url'], 'id': id})
+        else:
+            return jsonify({'error': 'Tag no encontrado'})
+
+@app.route('/tags/deletetag', methods=['GET','DELETE'])
+def deleteTag():
+    if request.method == 'DELETE':
+        id = request.form['id']
+        tags_col.delete_one({"_id": ObjectId(id)})
+        return redirect(url_for('deleteTag'))
+    data={
+        "title": "Eliminar Tag",
+        "db": tags
+    }
+    return render_template('/tags/delete.html', data=data)
+
+#----------------------------------------------------
+
 
 if __name__ == '__main__':
     app.run(debug=True)
